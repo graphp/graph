726dfd2bf7f36577408f5212ef20d0f3a32d6a69
Florian Nolden <fn187-github@yahoo.de>
2012-03-23 12:31:22 +0100

    added implementation of reading a edges from a graph.txt file

--- loaderEdgeList.php
+++ loaderEdgeList.php
@@ -1,7 +1,34 @@
 <?php
 
+include 'loader.php';
 class LoaderEdgeList implements Loader{
+	public function __construct(){
+		echo "Du bist im Konstruktor von LoaderEdgeList\n";
+		
+		//echo file_get_contents ("data/Graph2.txt" );
+		$zeilen = file ("data/Graph2.txt");
 	
+		foreach ($zeilen as $zeile) {
+			if($zeile!=$zeilen[0]){			
+			echo "hallo ";
+			$array = explode("\t",$zeile);
+			
+			//echo "beides ";
+			//echo $array;
+			echo "zeile ";
+			echo  $zeile;
+			echo "zahl 0 ";
+			echo $array[0];
+			echo "\nzahl 1 ";
+			echo $array[1];
+			}
+		}
+		
+	}
+	public function getGraphFromFile($fileName){
+		
+	}	
 }
+$interface = new LoaderEdgeList();
 
 ?>
