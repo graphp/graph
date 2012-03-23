446cb19d3197ab1ac85f1ef5be0453066d48c794
Chris <christian.lenski@alumni.fh-aachen.de>
2012-03-23 16:44:39 +0100

    Finished Example load adjacencyMatrix -> SearchDepthFirst

--- mainChristian.php
+++ mainChristian.php
@@ -1,16 +1,18 @@
 <?php
 include_once 'loaderAdjacencyMatrix.php';
-include_once 'vertice.php';
 
 $loaderAdjacencyMatrix = new LoaderAdjacencyMatrix();
 
 $derGraf = $loaderAdjacencyMatrix->getGraphFromFile("data/Graph1.txt");
 
-echo "\n";
 //foreach ($derGraf->getVertices() as $value) {
 //	echo $value->getId()." ";
 //}
 
 $derGraf->searchDepthFirst(1);
 
+// foreach ($derGraf->getEdgeArray() as $key => $value) {
+// 	echo $key;
+// 	print_r($value);
+// } 
 ?>
