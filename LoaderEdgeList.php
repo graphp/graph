1c873f2e83215f6830c86bd28390efd99bec2c47
Tobias Neumann <tobias@neumann-web.eu>
2012-03-30 01:08:57 +0200

    change to new interface methods of graph

--- LoaderEdgeList.php
+++ LoaderEdgeList.php
@@ -4,54 +4,33 @@ class LoaderEdgeList implements Loader{
 	
 	private $debugMode = false;
 	
+	private function writeDebugMessage($messageString){
+		if($this->debugMode){
+			echo $messageString;
+		}
+	}
+	
 	public function __construct(){
 	}
 	
 	public function getGraphFromFile($fileName){
-		//echo file_get_contents ("data/Graph2.txt" );
+		
 		$graph = new Graph();
-		$file = file($fileName);
+		
+		$file = file($fileName, FILE_IGNORE_NEW_LINES);
 		$vertexCount = $file[0];
-		
-		
-		// Add all vertices to the graph
-		$vertexArray=array();
-		for($i=0;$i<$vertexCount;$i++){
-			//echo "added vertize ".$i." \n";
-			$vertexArray[$i] = $graph->addVertex($i);	
-		}
-		
-		
-		// Add all edge informations from the file
 		$edgeCounter = 0;
-		foreach ($file as $zeile) {
-			//skip first entry, it contains only the number of vertices
-			if($zeile==$file[0]){
-				if ($this->debugMode){
-					echo "Starting with ".$zeile." elements\n";
-				}
-			}
-			else{
-				$array = explode("\t",$zeile);
-				//echo "trying to add edge nr {$edgeCounter} from {$array[0]} to {$array[1]}\n";
-				$edge = new EdgeUndirected($edgeCounter,$array[0],$array[1]);
-				// add edge to graph
-				$graph->addEdge($edge);
-				// add edge to vertex
-				$vertexArray[(int)$array[0]]->addEdgeId($edgeCounter);
-				$vertexArray[(int)$array[1]]->addEdgeId($edgeCounter);
-				if ($this->debugMode){
-					echo "added edge nr {$edgeCounter} from {$array[0]} to {$array[1]}\n";
-				}
-				$edgeCounter++;
-			}
-		}
-		for($i=0;$i<$vertexCount;$i++){
-			$graph->addVertex($vertexArray[$i]);
-		}
 		
-		if ($this->debugMode){
-			echo "Added {$vertexCount} vertices and {$edgeCounter} edges\n";
+		$graph->createVertices($vertexCount);
+		
+		unset($file[0]);
+		foreach ($file as $zeile) {
+			$edgeConections = explode("\t", $zeile);
+			
+			$from = $graph->getVertex($edgeConections[0]);
+			$to = $graph->getVertex($edgeConections[1]);
+			
+			$from->createEdge($to);								//TODO directed
 		}
 		
 		return $graph;
