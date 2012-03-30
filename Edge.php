e2b8ff788fc59b86a39e1f10edece5570d7d88f7
Christian Lück <christian@lueck.tv>
2012-03-30 12:17:15 +0200

    removed id

--- Edge.php
+++ Edge.php
@@ -1,16 +1,6 @@
 <?php
 
-abstract class Edge{
-	/**
-	 * returns the id of this Edge
-	 * 
-	 * @return int
-	 * @todo purpose? REMOVE ME?	=> Tobias: don't remove, ID could be used as a name (for a path its maybe good to know witch Edge it takes)
-	 */
-	public function getId(){
-		return 0;
-	}
-	
+abstract class Edge{	
 	abstract public function hasVertexTo($vertex);
 	
 	abstract public function hasVertexFrom($vertex);
