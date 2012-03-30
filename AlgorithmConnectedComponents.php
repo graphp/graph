87c0a73159a2580178db8cba91d2835eb8c46482
Tobias Neumann <tobias@neumann-web.eu>
2012-03-30 11:36:50 +0200

    improved comment

--- AlgorithmConnectedComponents.php
+++ AlgorithmConnectedComponents.php
@@ -18,24 +18,26 @@ class AlgorithmConnectedComponents{
 	
 	/**
 	 * @return int number of components
+	 * @uses Graph::getVertices()
+	 * @uses Vertex::searchBreadthFirst()
 	 */
 	public function getNumberOfComponents(){
 		$visitedVertices = array();
 		$components = 0;
 		
-		foreach ($this->graph->getVertices() as $vertex){			//for each vertices
-			if ( ! isset( $visitedVertices[$vertex->getId()] ) ){		//did I visit this vertex before?
+		foreach ($this->graph->getVertices() as $vertex){						//for each vertices
+			if ( ! isset( $visitedVertices[$vertex->getId()] ) ){					//did I visit this vertex before?
 				
-				$newVertices = $vertex->searchBreadthFirst();				//get all vertices of this component
+				$newVertices = $vertex->searchBreadthFirst();							//get all vertices of this component
 				
 				$components++;
 				
-				foreach ($newVertices as $v){								//mark the vertices of this component as visited
+				foreach ($newVertices as $v){											//mark the vertices of this component as visited
 					$visitedVertices[$v->getId()] = true;
 				}
 			}
 		}
 		
-		return $components;											//return number of components
+		return $components;														//return number of components
 	}
 }
