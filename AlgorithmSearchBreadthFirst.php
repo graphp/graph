d84d6cd894440e5e8f7af8d20ac3e11e35f7ba90
ToXeR <github@neumann-web.eu>
2012-03-30 17:05:36 +0200

    add comments for the allgorithm description

--- AlgorithmSearchBreadthFirst.php
+++ AlgorithmSearchBreadthFirst.php
@@ -11,22 +11,22 @@ class AlgorithmSearchBreadthFirst{
      */
     public function getVertices(){
         $queue = array($this->vertex);
-        $mark = array($this->vertex->getId() => true);
-        $visited = array();
+        $mark = array($this->vertex->getId() => true);							//to not add vertices twice in array visited
+        $visited = array();														//visited vertices
         
         do{
-            $t = array_shift($queue); // get first from queue
-            $visited[$t->getId()]= $t;
+            $t = array_shift($queue);												// get first from queue
+            $visited[$t->getId()]= $t;												//save as visited
             
-            $vertices = $t->getVerticesEdgeTo();
+            $vertices = $t->getVerticesEdgeTo();									//get next vertices
             foreach($vertices as $id=>$vertex){
-                if(!isset($mark[$id])){
-                    $queue[] = $vertex;
-                    $mark[$id] = true;
+                if(!isset($mark[$id])){													//if not "toughed" before
+                    $queue[] = $vertex;														//add to queue
+                    $mark[$id] = true;														//and mark
                 }
             }
         
-        }while($queue);
+        }while($queue);															//untill queue is empty
         
         return $visited;
     }
