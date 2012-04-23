426d3431e86f032f5c3268d65af92c9942965f5d
Chris <christian.lenski@alumni.fh-aachen.de>
2012-04-23 15:10:28 +0200

    Changed comments of TSP and nearest neighbour

--- lib/Algorithm/AlgorithmTSPMinimumSpanningTreeHeuristic.php
+++ lib/Algorithm/AlgorithmTSPMinimumSpanningTreeHeuristic.php
@@ -1,50 +1,51 @@
 <?php
 class AlgorithmTSPMinimumSpanningTreeHeuristic{
-	
+
 	private $graph;
-	
+
 	public function __construct(Graph $inputGraph){
- 		$this->graph = $inputGraph;
+		$this->graph = $inputGraph;
 	}
-	
+
 	/**
 	 *
 	 * @param Vertex $startVertex
 	 * @return Graph
 	 */
 	public function getResultGraph(){
-		$returnGraph = $this->graph->createGraphCloneEdgeless();				//Copy vertices of original graph
-		
-		$minimumSpanningTreeAlgorithm = new AlgorithmKruskal($this->graph);		//Create minimum spanning tree
+		$returnGraph = $this->graph->createGraphCloneEdgeless();				// Copy vertices of original graph
+
+		$minimumSpanningTreeAlgorithm = new AlgorithmKruskal($this->graph);		// Create minimum spanning tree
 		$minimumSpanningTree = $minimumSpanningTreeAlgorithm->getResultGraph();
-		
-		$depthFirstSearch = $minimumSpanningTree->getAnyVertex()->searchDepthFirst();	//Depth first search in minmum spanning tree (for the eulerian path)
-		
+
+		$depthFirstSearch = $minimumSpanningTree->getAnyVertex()->searchDepthFirst();	// Depth first search in minmum spanning tree (for the eulerian path)
+
 		$startVertex = NULL;
 		$oldVertex = NULL;
-		
-		foreach ($depthFirstSearch as $vertex){									//Connect vertices in order of the depth first search
-			
-			$vertex = $this->graph->getVertex( $vertex->getId() );					//get vertex from the original graph (not from the depth first search)
-																					//i need to clone the edge from the original graph, therefore i need the original edge
+
+		foreach ($depthFirstSearch as $vertex){									// connect vertices in order of the depth first search
+				
+			$vertex = $this->graph->getVertex( $vertex->getId() );				// get vertex from the original graph (not from the depth first search)
+																				// need to clone the edge from the original graph, therefore i need the original edge
 			if ($startVertex === NULL){
-				$startVertex = $vertex;
+				$startVertex = $vertex;											
 			}
 			else {
-				foreach ($oldVertex->getEdgesTo( $vertex ) as $edge ){				//Get edge to clone //more edges are possible (returns an array)
+				foreach ($oldVertex->getEdgesTo( $vertex ) as $edge ){			// get edge(s) to clone, multiple edges are possible (returns an array if undirected edge)
 					$returnGraph->createEdgeClone( $edge );
 					break;
 				}
 			}
-			
+				
 			$oldVertex = $vertex;
 		}
-
-		foreach ($oldVertex->getEdgesTo( $startVertex ) as $edge ){				//Connect last vertex with start vertex //retusn an array
+		
+																				// connect last vertex with start vertex
+		foreach ($oldVertex->getEdgesTo( $startVertex ) as $edge ){				// multiple edges are possible (returns an array if undirected edge)
 			$returnGraph->createEdgeClone( $edge );
 			break;
 		}
-		
+
 		return $returnGraph;
 	}
 }
