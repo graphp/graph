8de0c63146054f65e6f2d13192ec80c114d11152
Chris <christian.lenski@alumni.fh-aachen.de>
2012-05-06 18:25:21 +0200

    Proceeded with dijkstra algorithm Two (complex) //TODO entrys are still necessary

--- lib/Algorithm/AlgorithmSpDijkstra.php
+++ lib/Algorithm/AlgorithmSpDijkstra.php
@@ -12,30 +12,68 @@ class AlgorithmSpDijkstra{
 	public function getResultGraph(){
 
 		// Initialise programm
+		$returnGraph = $this->startGraph->createGraphCloneEdgeless();
+
 		$distanceFromStartTo  = Array();
-		$distanceFromStartTo[$this->startVertex] = 0;
+		$distanceFromStartTo[$this->startVertex->getId()] = 0;
 
 		$predecessorTo  = Array();
+		$predecessorTo[$this->startVertex->getId()] = $this->startVertex;
+
 		$marked  = Array();
 
-		$edgePriorityQueue = new SplPriorityQueue();
-		
+		$nodePriorityQueue = new SplPriorityQueue();
+
+		// Add reachable Nodes
 		foreach ($this->startVertex->getEdges() as $edge){
-			// Füge nur Kanten hinzu die von $this->startVertex abgehen
+			foreach ($edge->getVertexToFrom($this->startVertex) as $targetVertex){
+				$distanceFromStartTo[$targetVertex->getId()] = $edge->getWeight();
+				$predecessorTo[$targetVertex->getId()] = $this->startVertex;
+				$nodePriorityQueue->insert($targetVertex, -$edge->getWeight());
+			}
 		}
-		
-		
-		// Wiederhole bis alle vertices hinzugefügt
+
+
+		// Repeat until all vertices have been marked
 		$countOfVertices = 0;
 		$totalCountOfVertices = $this->startGraph->count();
 		while ($countOfVertices< $totalCountOfVertices){
 
-			// Hole aktuell am günstigsten erreicharen Knoten mittels edgePrioList
-				
-			// markiere diesen Knoten (der kürzeste weg ist bestimmt) & $countOfVertices++
-				
-			// Füge die Nachbarn von diesem Knoten hinzu -> set($distanceFromStartTo & $predecessorTo)
-			//		falls Nachbar bereit gesetzt war und neue Verbindung günstiger -> Update($distanceFromStartTo & $predecessorTo)
+			// Get next cheapest reachable node from priolist
+			$currentVertex = $nodePriorityQueue->extract();
+
+			$currentPredesessor = $predecessorTo[$currentVertex->getId()]; 			// mark predessessor (his cheapest path has been found)
+			if(!$marked[$currentPredesessor->getId()]){
+				$marked[$currentPredesessor->getId()] = true;
+				$countOfVertices++;													// only rise counter if fresh marked
+			}
+			
+
+			foreach ($currentVertex->getEdges() as $edge){ 											// Add reachable nodes from currently added node and refresh the current possible distances
+				foreach ($edge->getVertexToFrom($currentVertex) as $targetVertex){
+
+					$costsToTargetVertex = $distanceFromStartTo[$currentVertex->getId()] + $edge->getWeight();
+
+					if(!isset($distanceFromStartTo[$targetVertex->getId()])){						// if not yet added -> add
+						
+						$distanceFromStartTo[$targetVertex->getId()] = $costsToTargetVertex;
+						$predecessorTo[$targetVertex->getId()] = $currentVertex;
+						
+						$nodePriorityQueue->insert($targetVertex, -$costsToTargetVertex);
+						
+					} else if($distanceFromStartTo[$targetVertex->getId()] > $costsToTargetVertex){ // if new costs are lower -> update
+						
+						$distanceFromStartTo[$targetVertex->getId()] = $costsToTargetVertex;
+						$predecessorTo[$targetVertex->getId()] = $currentVertex;
+						
+						// TODO UPDATE VALUE IN PRIOQUEUE
+						// remove element from queue
+						// and add with new value
+					}
+				}
+			}
 		}
+		
+		// TODO Add correct edges to return graph or define better output (ex. costs and predesessor list)
 	}
 }
