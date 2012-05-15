12aaa198a7407bcd90c50edfb9d2ba962507a79b
Florian Nolden <fn187-github@yahoo.de>
2012-05-15 21:06:12 +0200

    updated MaxFlowEdmondsKarp algorithm. now works basically and returns the last residual graph.

--- lib/Algorithm/AlgorithmMaxFlowEdmondsKarp.php
+++ lib/Algorithm/AlgorithmMaxFlowEdmondsKarp.php
@@ -1,94 +1,157 @@
 <?php
 class AlgorithmMaxFlowEdmondsKarp{
-	
-	/**
-	 * 
-	 * @var Graph
-	 */
-	private $graph;
-	
-	/**
-	 * @var Vertex
-	 */
-	private $startVertex;
-	
-	/**
-	 * @var Vertex
-	 */
-	private $destinationVertex;
-	
-	/**
-	 * 
-	 * @param Vertex $startVertex the vertex where the flow search starts
-	 * @param Vertex $destinationVertex the vertex where the flow search ends the destination
-     */
-	public function __construct(Vertex $startVertex, Vertex $destinationVertex){
-		$this->startVertex = $startVertex;
-		$this->destinationVertex = $destinationVertex;
-		$this->graph = $startVertex->getGraph();
-	}
-	
-	private function start(){
-	    
-	    $currentGraph = $this->mergeParallelEdges($this->graph);                // remove parallel edges
-	    
-	    do{
-	        
-	        $path=$this->getGraphShortestPath($currentGraph);                   // Get Shortes path if NULL-> Done
-	        if($path){
-	            $currentGraph=$this->getResidualGraph($currentGraph, $path);
-	        }
-	    }
-	    while($path);
-	       return $this->getFlowGraphFromResidualGraph($currentGraph);
-	}
-	
-	
-	private function getFlowGraphFromResidualGraph($residualGraph){
-	    //TODO generate flow $returnGraph from $residualGraph and $this->graph
-    
-	    return new Graph();
-	}
-	
-	
-	
-	
-	private function mergeParallelEdges($currentGraph){
-	    //TODO 1. find and merge parallel edges
-	    return $currentGraph;
-	}
 
-	
-	private function getGraphShortestPath($currentGraph)
-	{
-	    // TODO 
-	    // 1. Search shortest path from s -> t
-	    $breadthSearchAlg = new AlgorithmSearchBreadthFirst($this->startVertex);
-	    $path = $breadthSearchAlg->getGraphPathTo($this->destinationVertex);
-	    
-	    // 2. get max flow from path
-	    // 3. create graph with shortest path and max flow as edge values
-	    return new graph;
-	}
-	
-	private function getResidualGraph($currentGraph, $path)
-	{
-	    //TODO 
-		// 1. Substract $path values from $graph
-		// 2. add in reversed direction of $path values to the $graph
-		return new graph();
-	}
-	
-	
-	
-	
-	/**
-	 * returns max flow graph
-	 *
-	 * @return Graph
-	 */
-	public function getResultGraph(){
-	   
-	    return $this->start();	
-	}
+    /**
+     *
+     * @var Graph
+     */
+    private $graph;
+
+    /**
+     * @var Vertex
+     */
+    private $startVertex;
+
+    /**
+     * @var Vertex
+     */
+    private $destinationVertex;
+
+    /**
+     *
+     * @param Vertex $startVertex the vertex where the flow search starts
+     * @param Vertex $destinationVertex the vertex where the flow search ends the destination
+     */
+    public function __construct(Vertex $startVertex, Vertex $destinationVertex){
+        $this->startVertex = $startVertex;
+        $this->destinationVertex = $destinationVertex;
+        $this->graph = $startVertex->getGraph();
+        $this->workingGraph = $this->graph->createGraphClone();
+    }
+
+    private function start(){
+         
+        
+        $currentGraph = $this->mergeParallelEdges($this->workingGraph);                // remove parallel edges
+        do{
+            $pathFlow = $this->getGraphShortestPathFlow($currentGraph);         // Get Shortes path if NULL-> Done
+             
+            if($pathFlow){
+                 $currentGraph = $this->getResidualGraph($currentGraph, $pathFlow);
+            }
+        } while($pathFlow);
+
+        return $this->getFlowGraphFromResidualGraph($currentGraph);
+    }
+
+
+    private function getFlowGraphFromResidualGraph($residualGraph){
+        //TODO generate flow $returnGraph from $residualGraph and $this->graph
+        //run over original graph and create a new graph with the flow 
+        echo "\nMissing function to remove not used edges and rotate the used edges.\n\n";
+
+        return $residualGraph;
+    }
+
+
+
+
+    private function mergeParallelEdges($currentGraph){
+        //TODO 1. find and merge parallel edges
+        // alg works only without prarallel edges
+        return $currentGraph;
+    }
+
+    /**
+     * get the shortest path flow (by count of edges)
+     *
+     * @param Graph $currentGraph
+     * @return Graph if path exists OR NULL
+     */
+    private function getGraphShortestPathFlow($currentGraph)
+    {
+        
+        $startVertex= $currentGraph->getVertex($this->startVertex->getId());
+        
+        // 1. Search shortest path from s -> t
+        $breadthSearchAlg = new AlgorithmSearchBreadthFirst($startVertex);
+        $path = $breadthSearchAlg->getGraphPathTo($this->destinationVertex);
+
+        
+        if(isset($path)){
+            // 2. get max flow from path
+            $maxFlowValue = $path->getMinEdgeValue();
+            if($maxFlowValue==0){
+                //echo "stop flow value is 0\n";
+                return null;
+            }
+             
+            // 3. create graph with shortest path and max flow as edge values
+            $path->setAllEdgeWeights($maxFlowValue);
+             
+            return $path;
+        }
+        else {
+            //no path found return null
+            return NULL;
+        }
+    }
+
+    private function getResidualGraph($currentGraph, $path)
+    {
+        // 1. Substract $path values from $graph
+        foreach($path->getEdges() as $flowEdge){
+
+            // find edge in original graph          
+     
+            $flowEdgeEndArray = $flowEdge->getTargetVertices(); 
+            $flowEdgeEndVertex =  array_shift($flowEdgeEndArray);
+            $currentGraphEndVertex = $currentGraph->getVertex($flowEdgeEndVertex->getId());
+
+            $flowEdgeStartArray = $flowEdge->getVertexFromTo($flowEdgeEndVertex);
+            $flowEdgeStartVertex = $flowEdgeStartArray;
+            $currentGraphStartVertex= $currentGraph->getVertex($flowEdgeStartVertex->getId());
+
+            
+            //lower the value of the original graph
+            $edgesToArray=$currentGraphStartVertex->getEdgesTo($currentGraphEndVertex);
+            $currentGraphEdge = array_shift($edgesToArray);
+        
+            $currentGraphEdge->setWeight($currentGraphEdge->getWeight()-$flowEdge->getWeight()); //substract weight
+            
+            
+
+            // 2. add in reversed direction of $path values to the $graph
+            
+            // Find out if reverse edge already exists
+            $edgeArray=$currentGraphEndVertex->getEdgesTo($currentGraphStartVertex);
+            $reverseEdge=array_shift($edgeArray);
+            if(!isset($reverseEdge)){
+                //no edge in reverese direction existing, create a new one
+                $reverseEdge=$currentGraphEndVertex->createEdgeTo($currentGraphStartVertex);
+                $reverseEdge->setWeight(0);
+            };
+            //add the weight to the reveresd edge
+            $reverseEdge->setWeight($reverseEdge->getWeight()+$flowEdge->getWeight());
+                       
+            //if the value of the original edge is 0, remove the edge
+            if ($currentGraphEdge->getWeight()==0){
+            	$currentGraph->removeEdge($currentGraphEdge);
+            }
+        }
+        return $currentGraph;
+    }
+
+
+
+
+    /**
+     * returns max flow graph
+     *
+     * @return Graph
+     */
+    public function getResultGraph(){
+
+        return $this->start();
+    }
 }
--- lib/Algorithm/AlgorithmSearchBreadthFirst.php
+++ lib/Algorithm/AlgorithmSearchBreadthFirst.php
@@ -4,20 +4,20 @@ class AlgorithmSearchBreadthFirst{
     public function __construct(Vertex $startVertex){
         $this->vertex = $startVertex;
     }
-    
+
     /**
-     * 
+     *
      * @return array[Vertex]
      */
     public function getVertices(){
         $queue = array($this->vertex);
         $mark = array($this->vertex->getId() => true);							//to not add vertices twice in array visited
         $visited = array();														//visited vertices
-        
+
         do{
             $t = array_shift($queue);												// get first from queue
             $visited[$t->getId()]= $t;												//save as visited
-            
+
             $vertices = $t->getVerticesEdgeTo();									//get next vertices
             foreach($vertices as $id=>$vertex){
                 if(!isset($mark[$id])){													//if not "toughed" before
@@ -25,70 +25,84 @@ class AlgorithmSearchBreadthFirst{
                     $mark[$id] = true;														//and mark
                 }
             }
-        
+
         }while($queue);															//untill queue is empty
-        
+
         return $visited;
     }
-    
+
     /**
-     * 
+     * Get a path from algorithms $startVertex to given $destination vertex
+     *
      * @param Vertex $destinationVertex
-     * @return Graph with lowest vertex count between start and destination
+     * @return Graph with lowest vertex count between start and destination OR NUll if no path exists
      */
     public function getGraphPathTo($destinationVertex){
-    	$queue = array($this->vertex);                                          //Start vertex
-    	$mark = array($this->vertex->getId() => true);							//to not add vertices twice in array visited
-    	
-    	$deepSearchThree = $this->vertex->getGraph()->createGraphCloneEdgeless();   //create copy of graph wihtout edges
-    	$visited = array();														//visited vertices
-    
-    	do{
-    		$t = array_shift($queue);											//get first from queue
-    		$visited[$t->getId()]= $t;											//save as visited
-    
-    		$vertices = $t->getVerticesEdgeTo();								//get next vertices
-    		foreach($vertices as $id=>$vertex){
-    			if(!isset($mark[$id])){											//if not "toughed" before
-    				$queue[] = $vertex;											//add to queue
-    				$mark[$id] = true;			                                //and mark
-    															
-    				$deepSearchThree->addEdge($deepSearchThree->getVertex($t->getId())->createEdgeTo($vertex));
-    			}
-    		}
-    		
-    		if(isset($mark[$destinationVertex])){                               //stop if destination node is reached
-    		    $queue=null;
-    		}
-    
-    	}while($queue);															//untill queue is empty
-    	
-    	//remove not used edges
-    	$destinationVertexInNewGraph = $deepSearchThree->getVertex($destinationVertex->getId());
-    	
-    	
-    	// create path
-        $path = new Graph();
-        $path->createVertexClone($destinationVertexInNewGraph);                 //add clone of destination vertex to path
-        $currentVertex=$destinationVertexInNewGraph;
+        $originGraph=$this->vertex->getGraph();
         
-        
-        do{                                                                     // run from the buttom to top to the tree and add the predecessor until the startvertex is reached
-            $edge=$currentVertex->getIngoingEdges();                            // can only one because of the deepsearch tree
-            
-            $upperVertex=$edge[0]->getVertexFromTo($currentVertex);
-            $path->createVertexClone($upperVertex);                             // clone upper vertex to graph
-            $path->createEdgeClone($edge[0]);                                   // clone connecting edge
-            $currentVertex=$upperVertex;
+        $queue = array($this->vertex);                                          //Start vertex
+        $mark = array($this->vertex->getId() => true);							//to not add vertices twice in array visited
+         
+        $deepSearchThree = $this->vertex->getGraph()->createGraphCloneEdgeless();   //create copy of graph wihtout edges
+        $visited = array();														//visited vertices
 
+        $pathToDestinationVertexFound = false;
+
+        do{
+            $t = array_shift($queue);											//get first from queue
+            $visited[$t->getId()]= $t;											//save as visited
+            
+            $vertices = $t->getVerticesEdgeTo();								//get next vertices
+            foreach($vertices as $id=>$vertex){
+                if(!isset($mark[$id])){											//if not "toughed" before
+                    $queue[] = $vertex;											//add to queue
+                    $mark[$id] = true;			                                //and mark
+                     
+                    $oldEdgeArray=$t->getEdgesTo($vertex);
+                    $oldEdge=array_shift($oldEdgeArray);
+                    
+                    $newEdge=$deepSearchThree->createEdgeClone($oldEdge);
+                    
+                }
+            }
+
+            if(isset($mark[$destinationVertex->getId()])){                               //stop if destination node is reached
+                $queue=null;
+                $pathToDestinationVertexFound = true;
+            }
+
+        } while($queue);															//untill queue is empty
+         
+        if($pathToDestinationVertexFound){
+
+            //remove not used edges
+            $destinationVertexInNewGraph = $deepSearchThree->getVertex($destinationVertex->getId());
+             
+             
+            // create path
+            $path = new Graph();
+            $path->createVertexClone($destinationVertex);                 //add clone of destination vertex to path
+            $currentVertex=$destinationVertexInNewGraph;
+
+            //run over the tree
+            do{                                                                     // run from the buttom to top to the tree and add the predecessor until the startvertex is reached
+                $edge=$currentVertex->getIngoingEdges();                            // can only one because of the deepsearch tree
+
+                $upperVertex=$edge[0]->getVertexFromTo($currentVertex);
+                $path->createVertexClone($upperVertex);                             // clone upper vertex to graph
+                $path->createEdgeClone($edge[0]);                                   // clone connecting edge
+             
+                $currentVertex=$upperVertex;
+
+            } while($currentVertex->getId()!==$this->vertex->getId());
+
+            return $path;
+
+        } else {
+            return NULL;
         }
-        while($currentVertex->getId()!==$this->vertex->getId());
-        
-       
-    
-    	return $path;
     }
-    
+
     public function getVerticesIds(){
         return array_keys($this->getVertices());
     }
