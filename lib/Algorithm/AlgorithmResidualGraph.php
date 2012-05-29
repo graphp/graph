3866d4f09d02689206bc6ad80bb085483e61e7c7
Christian Lück <christian@lueck.tv>
2012-05-29 11:48:56 +0200

    Algorithm to create residual graphs

---
+++ lib/Algorithm/AlgorithmResidualGraph.php
@@ -0,0 +1,54 @@
+<?php
+
+class AlgorithmResidualGraph extends Algorithm{
+    private $graph;
+    private $keepNullCapacity = false;
+    
+    public function __construct(Graph $graph){
+        $this->graph = $graph;
+    }
+    
+    public function setKeepNullCapacity($toggle){
+        $this->keepNullCapacity = !!$toggle;
+        return $this;
+    }
+    
+    /**
+     * create redisual graph
+     * 
+     * @throws Exception if input graph has undirected edges or flow/capacity is not set
+     * @return Graph
+     * @uses Graph::createGraphCloneEdgeless()
+     * @uses Graph::createEdgeClone()
+     * @uses Graph::createEdgeCloneInverted()
+     */
+    public function getResultGraph(){
+        $newgraph = $this->graph->createGraphCloneEdgeless();
+        foreach($this->graph->getEdges() as $edge){
+            if(!($edge instanceof EdgeDirected)){
+                throw new Exception('Edge is undirected');
+            }
+            $flow     = $edge->getFlow();
+            if($flow === NULL){
+            	throw new Exception('Flow not set');
+            }
+            $capacity = $edge->getCapacity();
+            if($capacity === NULL){
+                throw new Exception('Capacity not set');
+            }
+            
+            // capacity is still available, clone remaining capacity into new edge
+            if($this->keepNullCapacity || $flow < $capacity){
+                $newgraph->createEdgeClone($edge)->setFlow(0)
+                                                 ->setCapacity($capacity - $flow);
+            }
+            
+            // flow is set, clone current flow as capacity for back-flow into new inverted edge (opposite direction) 
+            if($this->keepNullCapacity || $flow > 0){
+                $newgraph->createEdgeCloneInverted($edge)->setFlow(0)
+                                                         ->setCapacity($flow);
+            }
+        }
+        return $newgraph;
+    }
+}
