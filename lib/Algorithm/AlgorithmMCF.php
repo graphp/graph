36c491d3bd47be7a8fa005f6cbec8d847100646a
Florian Nolden <fn187-github@yahoo.de>
2012-05-30 16:14:34 +0200

    updated AlgorithmMCF to use the initFlow function of the Graph object

--- lib/Algorithm/AlgorithmMCF.php
+++ lib/Algorithm/AlgorithmMCF.php
@@ -10,6 +10,12 @@ abstract class AlgorithmMCF extends Algorithm {
 	protected $graph;
 	
 	/**
+	 * 
+	 * @var unknown_type
+	 */
+	protected $resultGraph;
+	
+	/**
 	 * The given graph where the algorithm should operate on
 	 * 
 	 * @param Graph $graph
@@ -24,10 +30,15 @@ abstract class AlgorithmMCF extends Algorithm {
     	foreach ($vertices as $vertex) {										//Sum for all vertices of value
     		$balance += $vertex->getValue();
     	}
-    	if ($balance !== 0) {													//If the sum is 0 => same "in-flow" as "out-flow"
-    		throw new Exception("The given graph is not balanced");
+    	if (($balance > 0 || $balance < 0)) {													//If the sum is 0 => same "in-flow" as "out-flow"
+    		throw new Exception("The given graph is not balanced value is: ".$balance);
     	}
+    	$resultGraph = $this->graph->createGraphClone();
+    	//preset the flow with the capacity values
+    	$resultGraph->initFlow();
     }
+    
+   
     
     /**
      * create new resulting graph with minimum-cost flow on edges
--- lib/Algorithm/AlgorithmMCFCycleCanceling.php
+++ lib/Algorithm/AlgorithmMCFCycleCanceling.php
@@ -4,24 +4,15 @@ class AlgorithmMCFCycleCanceling extends AlgorithmMCF {
 	
 	public function getResultGraph() {
 		
-		$resultGraph = $this->graph->createGraphClone();
+		
 		//initial-zustand setzten, 0 für Positiv gewichtete Kanten max für negaitv gewichtete Kanten
 		
-		$edges = $resultGraph->getEdges();										//initial flow of edges
-		foreach ($edges as $edge){
-			$flow = 0;															//0 if weight of edge is positiv
-			
-			if ($edge->getWeight() < 0){										//maximal flow if weight of edge is negative
-				$flow = $edge->getCapacity();
-			}
-			
-			$edge->setFlow($flow);
-		}
 		
 		//while no more paths
 		{
-			//1. calculate b-flow
-			//    add supersource and supersink 
+		    //1. calculate b-flow
+		    //    find sinks and sources	    
+			//    create supersource and supersink 
 			//    max flow searched
 			//2. Create a Residualgraph 
 			//    get the residual capacity and the	Residualcosts
@@ -33,7 +24,9 @@ class AlgorithmMCFCycleCanceling extends AlgorithmMCF {
 
 		}
 		
-		return $resultGraph;
+		return $this->resultGraph;
 	}
+	
+
 	
 }
