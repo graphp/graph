8d2dfc1f16df0a72673d4663f2ccf4458aabeaa0
Christian Lück <christian@lueck.tv>
2012-06-03 12:03:57 +0200

    Throw exception for edges with negative weight

--- lib/Algorithm/AlgorithmSpDijkstra.php
+++ lib/Algorithm/AlgorithmSpDijkstra.php
@@ -41,12 +41,17 @@ class AlgorithmSpDijkstra extends AlgorithmSp{
             $usedVertices[$currentVertexId] = true;                                //mark this vertex
                 
             foreach ($currentVertex->getEdgesOut() as $edge){                 //check for all edges of current vertex if there is a cheaper path (or IN OTHER WORDS: Add reachable nodes from currently added node and refresh the current possible distances)
+                $weight = $edge->getWeight();
+                if($weight < 0){
+                	throw new Exception('Djkstra not supported for negative weights - Consider using MooreBellmanFord');
+                }
+                
                 $targetVertex = $edge->getVertexToFrom($currentVertex);
                 $targetVertexId = $targetVertex->getId();
 
                 if ( ! isset( $usedVertices[$targetVertexId] ) )                //if the targetVertex is marked, the cheapest path for this vertex has already been found (no negative edges)
                 {
-                    $newCostsToTargetVertex = $totalCostOfCheapestPathTo[$currentVertexId] + $edge->getWeight();    //calculate new cost to vertex
+                    $newCostsToTargetVertex = $totalCostOfCheapestPathTo[$currentVertexId] + $weight;    //calculate new cost to vertex
                         
                     if ( ( ! isset($predecesVertexOfCheapestPathTo[$targetVertexId]) )
                            || $totalCostOfCheapestPathTo[$targetVertexId] > $newCostsToTargetVertex){    //is the new path cheaper?
