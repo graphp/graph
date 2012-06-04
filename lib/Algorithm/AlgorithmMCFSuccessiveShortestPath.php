51b2ecd722c664ed99c89eda5980d42636cbb84a
ToXeR <github@neumann-web.eu>
2012-06-04 18:07:58 +0200

    including helping functions getVertexSource -Sink into algorithm

--- lib/Algorithm/AlgorithmMCFSuccessiveShortestPath.php
+++ lib/Algorithm/AlgorithmMCFSuccessiveShortestPath.php
@@ -17,7 +17,7 @@ class AlgorithmMCFSuccessiveShortestPath extends AlgorithmMCF {
         //initial flow of edges
         $edges = $resultGraph->getEdges();
         foreach ($edges as $edge){
-            $flow = 0;                                                            //0 if weight of edge is positiv
+            $flow = 0;                                                          //0 if weight of edge is positiv
             
             if ($edge->getWeight() < 0){                                        //maximal flow if weight of edge is negative
                 $flow = $edge->getCapacity();
@@ -26,39 +26,22 @@ class AlgorithmMCFSuccessiveShortestPath extends AlgorithmMCF {
             $edge->setFlow($flow);
         }
         
-        while(true)                                                                //return or Exception insite this while
+        while(true)                                                             //return or Exception insite this while
         {
             //create residual graph
             $algRG = new AlgorithmResidualGraph($resultGraph);
             $residualGraph = $algRG->createGraph();
             
             //search for a source    
-            $vertices = $residualGraph->getVertices();
+            $sourceVertex = $this->getVertexSource($residualGraph);
             
-            $sourceVertex = null;
-            foreach ($vertices as $vertex){                                        //forall (just use the first found)
-                
-                if ($vertex->getFlow() > 0){                                    //if flow of vertex is positiv => source
-                    $sourceVertex = $vertex;
-                    break;
-                }
-            }
             if ($sourceVertex === null){                                        //if no source is found the minimum-cost flow is found 
                 return $resultGraph;
             }
             
             //search for reachble target from this source
-            $algBFS = new AlgorithmSearchBreadthFirst($sourceVertex);            //search for reachable Vertices
-            $vertices = $algBFS->getVertices();
+            $this->getVertexSink($sourceVertex);
             
-            $targetVertex = null;
-            foreach ($vertices as $vertex){                                        //forall (just use the first found)
-                
-                if ($vertex->getFlow() < 0){                                    //if flow of vertex is negative => target
-                    $targetVertex = $vertex;
-                    break;
-                }
-            }
             if ($targetVertex === null){                                        //if no target is found the network has not enough capacity
                 throw new Exception("The graph has not enough capacity for the minimum-cost flow");
             }
@@ -115,18 +98,20 @@ class AlgorithmMCFSuccessiveShortestPath extends AlgorithmMCF {
                 return $vertex;
             }
         }
-        throw new Exception('No source vertex found in graph');
+        return null;
+        //throw new Exception('No source vertex found in graph');
     }
     
     private function getVertexSink(Vertex $source){
-        $algBFS = new AlgorithmSearchBreadthFirst($source);            //search for reachable Vertices
+        $algBFS = new AlgorithmSearchBreadthFirst($source);                     //search for reachable Vertices
         
         foreach($algBFS->getVertices() as $vid=>$vertex){
             if($this->graph->getVertex($vid)->getBalance() < $vertex->getBalance()){
                 return $vertex;
             }
         }
-        throw new Exception('No sink vertex connected to given source vertex found');
+        return null;
+        //throw new Exception('No sink vertex connected to given source vertex found');
     }
     
 }
