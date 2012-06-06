b964aba5ef9b76edab3d53a7a09680a54d8eadcd
Christian Lück <christian@lueck.tv>
2012-06-07 00:28:42 +0200

    Re-use MBF to detect negative cycles

--- lib/Algorithm/AlgorithmDetectNegativeCycle.php
+++ lib/Algorithm/AlgorithmDetectNegativeCycle.php
@@ -10,120 +10,54 @@ class AlgorithmDetectNegativeCycle extends Algorithm{
 
     /**
      *
-     * @var Vertex
-     */
-    private $startVertex;
-
-    /**
-     *
      * @param Graph $graph
-     * @param Vertex $startVertex
      */
-    public function __construct(Graph $graph, Vertex $startVertex){
+    public function __construct(Graph $graph){
         $this->graph = $graph;
-        $this->startVertex = $startVertex;
     }
-
-    //TODO this function
+    
     /**
-     * Depth-first-search for a negative cycle
-     *
-     * @param Vertex $vertex
-     * @param array $visitedVertices
-     *
-     * @return Graph the result graph if a negative cycle is found or NULL
+     * check if the input graph has any negative cycles
+     * 
+     * @return boolean
+     * @uses AlgorithmDetectNegativeCycle::getCycleNegative()
      */
-    private function searchNextDepth(Vertex $currentVertex, array $visitedVertices, array $predessesors){
-
-        if ( isset($visitedVertices[$currentVertex->getId()]) ){                       //cycle
-            $id = $currentVertex->getId();
-
-            //checke ob negativer cycle
-            $cycle = $currentVertex->getGraph()->createGraphCloneEdgeless();
-            $weight=0;
-            $tempVertex=$currentVertex;
-            do{
-                $predesssorVertex = $predessesors[$tempVertex->getId()];
-                $edges = $predesssorVertex->getEdgesTo($tempVertex);
-                $edge = $edges[0];
-                $weight = $weight + $edge->getWeight();
-                $cycle->createEdgeClone($edge);
-                $tempVertex=$predesssorVertex;
-            }
-            while ($currentVertex !== $predesssorVertex);
-            //baue graph zusammen
-            //gebe graph zurück
-            if($weight<0){
-                return $cycle;
-            }
-            return NULL;
-            //else continue with search
+    public function hasCycleNegative(){
+        try{
+            $this->getCycleNegative();
+            return true;                                                      // cycle was found => okay
         }
-
-        $visitedVertices[$currentVertex->getId()] = true;
-
-        $vertices = $currentVertex->getVerticesEdgeTo();                    //get next level of vertices
-        foreach ($vertices as $vertex){                                            //checke for all vertices if the found the negative cycle
-            $predessesors[$vertex->getId()]=$currentVertex;
-            $graph = $this->searchNextDepth($vertex, $visitedVertices, $predessesors);
-
-            if ($graph != NULL){                                                //If they found the negative cycle return the result graph
-                return $graph;
-            }
-        }
-
-        return NULL;                                                            //otherwise return NULL
+        catch(Exception $ignore){}                                             // no cycle found
+        return false;
     }
-
- /**
-     * Searches backwords for a negative cycle
-     *
-     * @param Vertex $startVertex optional
-     *
-     * @return Graph with the negative cycle
-     *
-     * @throws Exception if there isn't a negative cycle
-     */
-    public function getNegativeCycleForStartVertex(){
-
-        $visitedVertices = array();
-        $predessesors= array();
-        $visitedVertices[$this->startVertex->getId()] = true;                        //Visited Vertices with the cost to ???
-
-        $vertices = $this->startVertex->getVerticesEdgeTo();                    //get next level of vertices
-
-        foreach ($vertices as $vertex){                                            //checke for all vertices if the found the negative cycle
-            $predessesors[$vertex->getId()] = $this->startVertex;
-            $graph = $this->searchNextDepth($vertex, $visitedVertices, $predessesors);
-
-            if ($graph != NULL){                                                //If they found the negative cycle return the result graph
-                return $graph;
-            }
-        }
-
-        throw  new Exception("No negative cycle found");
-    }
-
+    
     /**
      * Searches all vertices for the first negative cycle
      *
-     * @return Graph with the negative cycle
-     *
+     * @return NegativeCycleException
      * @throws Exception if there isn't a negative cycle
+     * @uses AlgorithmSpMooreBellmanFord::getCycleNegative()
      */
-    public function getNegativeCycle(){
-
-        $visitedVertices = array();
-        foreach ($this->graph->getVertices() as $currentVertex){
-            $this->startVertex=$currentVertex;
-            try {
-                return $this->getNegativeCycleForStartVertex();
-            } catch (Exception $e) {
-
+    public function getCycleNegative(){
+        foreach($this->graph->getVertices() as $vertex){                      // check for all vertices
+            $alg = new AlgorithmSpMooreBellmanFord($vertex);                   // start MBF algorithm on current vertex
+            try{
+                return $alg->getCycleNegative();                               // try to get negative cycle for current vertex
             }
-
-        }
-        throw  new Exception("No negative cycle found");
+            catch(Exception $ignore){ }                                        // no cycle found, check next vertex...
+        }                                                                       // no more vertices to check => abort
+        throw new Exception("No negative cycle found");
     }
-
+    
+    /**
+     * create new graph clone with only vertices and edges in negative cycle
+     * 
+     * @return Graph
+     * @throws Exception if there's no negative cycle
+     * @uses AlgorithmDetectNegativeCycle::getCycleNegative()
+     * @uses NegativeCycleException::createGraph()
+     */
+    public function createGraph(){
+        return $this->getCycleNegative()->createGraph();
+    }
 }
