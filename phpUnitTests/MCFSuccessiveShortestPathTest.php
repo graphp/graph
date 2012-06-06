627391066d19f0304f09101ba86bf0efafdf4294
Chris <christian.lenski@alumni.fh-aachen.de>
2012-06-06 10:13:22 +0200

    Extended UniTests

---
+++ phpUnitTests/MCFSuccessiveShortestPathTest.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'example/init.inc.php';
+
+class MCFSuccessiveShortestPathTest extends PHPUnit_Framework_TestCase
+{
+    private $testedGraph = null;
+
+    // Run this code without crashing
+    public function testRunningAlgorithm()
+    {
+        $file = "Kostenminimal5.txt";
+
+        $loader = new LoaderEdgeListWithWeightedCapacityAndBalance(PATH_DATA.$file);
+        $loader->setEnableDirectedEdges(true);
+
+        $steffiGraf = $loader->createGraph();
+
+        $alg = new AlgorithmMCFSuccessiveShortestPath($steffiGraf);
+        $newGraph =  $alg->createGraph();
+
+        $this->assertEquals(-12, $alg->getWeightFlow());
+    }
+}
