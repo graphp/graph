ac4daa93814e9d1caf7eb2ade907885dfb1c0509
Christian Lück <christian@lueck.tv>
2012-06-10 01:57:53 +0200

    Dummy tests

---
+++ phpUnitTests/GraphTest.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once 'example/init.inc.php';
+
+class GraphTest extends PHPUnit_Framework_TestCase
+{
+    public function setup(){
+        $this->graph = new Graph();
+    }
+    
+    public function testBalance(){
+        $this->assertEquals(0,$this->graph->getBalance());
+    }
+    
+    public function testGetWeightMin(){
+        $this->assertEquals(NULL,$this->graph->getWeightMin());
+    }
+    
+    public function testCanCreateVertex(){
+        $this->graph->createVertex(0);
+    }
+
+// commented out because expecting base Exception is not allowed
+//     /**
+//      * @expectedException Exception
+//      */
+//     public function testCanNotCreateVertexDuplicateId(){
+//         $this->graph->createVertex(1);
+//         $this->graph->createVertex(1);
+//     }
+    
+    
+}
