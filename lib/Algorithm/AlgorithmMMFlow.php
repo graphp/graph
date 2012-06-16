856e71263788c3621295c21c3356a36efac72c3b
Christian Lück <christian@lueck.tv>
2012-06-16 20:35:41 +0200

    Use vertex groups and validate groups are actually bipartit

--- lib/Algorithm/AlgorithmMMFlow.php
+++ lib/Algorithm/AlgorithmMMFlow.php
@@ -2,36 +2,50 @@
 class AlgorithmMMFlow extends AlgorithmMM {
 
     public function createGraph() {
+//         if($this->graph->isDirected()){
+//             throw new Exception('Input graph must not be directed');
+//         }
+        
+        $alg = new AlgorithmGroups($this->graph);
+        if(!$alg->isBipartit()){
+            throw new Exception('Input graph does not have bipartit groups assigned to each vertex. Consider Using "AlgorithmBipartit::createGraph()" first');
+        }
+        
         // create resulting graph with supersource and supersink
         $resultGraph = $this->graph->createGraphClone();
          
         $maxMatchingValue = $resultGraph->getNumberOfEdges();
 
+        $vertices = $resultGraph->getVertices(); // get all vertices
+        // above $vertices does NOT contain supersource and supersink, because
+        // we want to skip over them as they do not have a partition assigned
+        
         $superSource = $resultGraph->createVertex()->setLayout('label','s*');
         $superSink   = $resultGraph->createVertex()->setLayout('label','t*');
-         
+        
+        $groups = $alg->getGroups();
+        $groupA = $groups[0];
+        $groupB = $groups[1];
+        
         // connect supersource s* to set A and supersink t* to set B
-        foreach($resultGraph->getVertices() as $vertex){
-            $layout = $vertex->getLayout();
-            $belongingSet = $layout['label'];
-
-            $result = strcmp($belongingSet, "\"A\"");
-            if(strcmp($belongingSet, "\"A\"") == 0){ // source
+        foreach($vertices as $vertex){
+            $group = $vertex->getGroup();
+            
+            if($group === $groupA){ // source
                 $superSource->createEdgeTo($vertex)->setCapacity($maxMatchingValue);
-            } else if(strcmp($belongingSet, "\"B\"") == 0){ // sink
+            } else if($group === $groupB){ // sink
                 $vertex->createEdgeTo($superSink)->setCapacity($maxMatchingValue);
-            } else if(strcmp($belongingSet, "\"s*\"") == 0 || strcmp($belongingSet, "\"t*\"") == 0){
-                // ignore supersource and supertarget
-            }
-            else {
+            } else {
                 throw new Exception('Unknown set: ' + $belongingSet);
             }
         }
 
         // All capacities to 1 (according to algorithm)
         foreach ($resultGraph->getEdges() as $edge){
-            $edge->setCapacity(1);
+            $edge->setCapacity(1)->setFlow(0);
         }
+        
+//         visualize($resultGraph);
 
         // calculate (s*,t*)-flow
         $algMaxFlow = new AlgorithmMaxFlowEdmondsKarp($superSource,$superSink);
@@ -40,27 +54,15 @@ class AlgorithmMMFlow extends AlgorithmMM {
         // destroy temporary supersource and supersink again
         $resultGraph->getVertex($superSink->getId())->destroy();
         $resultGraph->getVertex($superSource->getId())->destroy();
-
-        // Reset labels
-        foreach($resultGraph->getVertices() as $vertex){
-            $vertex->setLayout('label', null);
-
-        }
-
-        
-
-        return $resultGraph;
-    }
-    
-    
-    public function createGraphMatchingOnly() {
-        
-        $resultGraph = $this->createGraph();
         
         // Remove non matchings
         foreach($resultGraph->getEdges() as $edge){
         	if($edge->getFlow() == 0) {
         		$edge->destroy();
+        	} else {
+        	    $edgeOriginal = $this->graph->getEdgeClone($edge);
+        	    $edge->setCapacity($edgeOriginal->getCapacity());
+        	    $edge->setFlow($edgeOriginal->getFlow());
         	}
         }
         
