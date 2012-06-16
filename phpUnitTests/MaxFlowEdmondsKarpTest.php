7bf84f21d4e45317c1b8b650f624607d1a618f35
Christian Lück <christian@lueck.tv>
2012-06-17 01:27:07 +0200

    Additional test for bigger input graph

--- phpUnitTests/MaxFlowEdmondsKarpTest.php
+++ phpUnitTests/MaxFlowEdmondsKarpTest.php
@@ -5,21 +5,35 @@ use Fhaculty\Graph\Loader\EdgeListWithCapacity as LoaderEdgeListWithCapacity;
 
 class MaxFlowEdmondsKarpTest extends PHPUnit_Framework_TestCase
 {
-    private $testedGraph = null;
-
-    // Run this code without crashing
-    public function testRunningAlgorithm()
-    {
-        $file = "Fluss.txt";
-
+    private function readGraph($file){
         $LoaderEdgeListWithCapacity = new LoaderEdgeListWithCapacity(PATH_DATA.$file);
         $LoaderEdgeListWithCapacity->setEnableDirectedEdges(true);
+        
+        return $LoaderEdgeListWithCapacity->createGraph();
+    }
+    
+    /**
+     * run algorithm with small graph and check result against known result
+     */
+    public function testKnownResultSmall()
+    {
+        $graph = $this->readGraph('Fluss.txt');
 
-        $this->testedGraph = $LoaderEdgeListWithCapacity->createGraph();
-
-        $alg = new AlgorithmMaxFlowEdmondsKarp($this->testedGraph->getVertex(0),$this->testedGraph->getVertex(7));
-        $newGraph =  $alg->createGraph();
-
+        $alg = new AlgorithmMaxFlowEdmondsKarp($graph->getVertex(0),$graph->getVertex(7));
+        
         $this->assertEquals(4, $alg->getFlowMax());
     }
+    
+    /**
+     * run algorithm with bigger graph and check result against known result (will take several seconds)
+     */
+    public function testKnownResultBig(){
+        
+        $graph = $this->readGraph('G_1_2.txt');
+        
+        $alg = new AlgorithmMaxFlowEdmondsKarp($graph->getVertex(0),$graph->getVertex(4));
+        
+        $this->assertEquals(0.735802,$alg->getFlowMax());
+    }
+    
 }
