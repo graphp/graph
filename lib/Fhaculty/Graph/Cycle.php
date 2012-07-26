17bf47869f003db6445e0b41f62ddd3b54fb03b3
Christian Lück <christian@lueck.tv>
2012-07-27 01:21:36 +0200

    Temporarily base cycle off of walk and remove duplicate logic

--- lib/Fhaculty/Graph/Cycle.php
+++ lib/Fhaculty/Graph/Cycle.php
@@ -2,21 +2,7 @@
 
 namespace Fhaculty\Graph;
 
-class Cycle{
-	/**
-	 * array of vertices in the cycle
-	 * 
-	 * @var array[Vertex]
-	 */
-    private $vertices;
-    
-    /**
-     * array of edges in the cycle
-     * 
-     * @var array[Edge]
-     */
-    private $edges;
-    
+class Cycle extends Walk{    
     /**
      * create new cycle instance from given predecessor map
      * 
@@ -110,21 +96,7 @@ class Cycle{
         
         return new Cycle($vertices,$edges);
     }
-
-	private function __construct($vertices,$edges){
-		$this->vertices = $vertices;
-		$this->edges    = $edges;
-	}
-
-	/**
-	 * get all vertices in the cycle (no duplicate vertices)
-	 *
-	 * @return array[Vertex] 1+ vertices
-	 */
-	public function getVertices(){
-		return $this->vertices;
-	}
-
+    
 	/**
 	 * get IDs of all vertices in the cycle
 	 *
@@ -132,57 +104,5 @@ class Cycle{
 	 */
 	public function getVerticesId(){
 		return array_keys($this->vertices);
-	}
-
-	/**
-	 * get all edges in the cycle (A->B->C->A)
-	 *
-	 * @return array[Edge] 1+ edges
-	 */
-	public function getEdges(){
-		return $this->edges;
-	}
-
-	/**
-	 * get total weight of cycle (sum all edges' weights)
-	 *
-	 * @return float
-	 */
-	public function getWeight(){
-		$sum = 0;
-		foreach($this->edges as $edge){
-			$sum += $edge->getWeight();
-		}
-		return $sum;
-	}
-	
-	/**
-	 * get original graph of this cycle
-	 * 
-	 * @return Graph
-	 * @throws LogicException
-	 */
-	public function getGraph(){
-	    foreach($this->vertices as $vertex){
-	        return $vertex->getGraph();
-	    }
-	    throw new Exception\LogicException('No vertex found. Must not happen');
-	}
-	
-	/**
-	 * create new graph clone with only vertices and edges actually in the cycle
-	 * 
-	 * @return Graph
-	 * @uses Cycle::getEdges()
-	 * @uses Graph::createGraphCloneEdges()
-	 */
-	public function createGraph(){
-	    $graph = $this->getGraph()->createGraphCloneEdges($this->edges);        // create new graph clone with only cycle edges
-	    foreach($graph->getVertices() as $vid=>$vertex){                      // get all vertices
-	        if(!isset($this->vertices[$vid])){
-	            $vertex->destroy();                                             // remove those not present in the cycle (isolated vertices)
-	        }
-	    }
-	    return $graph;
 	}
 }
