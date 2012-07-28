26eeb31138b45f53563c0ba4df34707c821bfcca
Christian Lück <christian@lueck.tv>
2012-07-28 11:57:01 +0200

    Simple unit tests for edmonds karp

--- phpUnitTests/MaxFlowEdmondsKarpTest.php
+++ phpUnitTests/MaxFlowEdmondsKarpTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Fhaculty\Graph\Graph;
+
 use Fhaculty\Graph\Algorithm\MaxFlow\EdmondsKarp as AlgorithmMaxFlowEdmondsKarp;
 use Fhaculty\Graph\Loader\EdgeListWithCapacity as LoaderEdgeListWithCapacity;
 
@@ -23,6 +25,58 @@ class MaxFlowEdmondsKarpTest extends PHPUnit_Framework_TestCase
         
         $this->assertEquals(4, $alg->getFlowMax());
     }
+    
+    public function testEdgeDirected(){
+        $graph = new Graph();
+        $v0 = $graph->createVertex(0);
+        $v1 = $graph->createVertex(1);
+        
+        $v0->createEdgeTo($v1)->setCapacity(10);
+        
+        $alg = new AlgorithmMaxFlowEdmondsKarp($v0,$v1);
+        
+        $this->assertEquals(10,$alg->getFlowMax());
+    }
+    
+    public function testEdgesMultiplePaths(){
+    	$graph = new Graph();
+    	$v0 = $graph->createVertex(0);
+    	$v1 = $graph->createVertex(1);
+    	$v2 = $graph->createVertex(2);
+    
+    	$v0->createEdgeTo($v1)->setCapacity(5);
+    	$v0->createEdgeTo($v2)->setCapacity(7);
+    	$v2->createEdgeTo($v1)->setCapacity(9);
+    
+    	$alg = new AlgorithmMaxFlowEdmondsKarp($v0,$v1);
+    
+    	$this->assertEquals(12,$alg->getFlowMax());
+    }
+    
+//     public function testEdgesParallel(){
+//         $graph = new Graph();
+//         $v0 = $graph->createVertex(0);
+//         $v1 = $graph->createVertex(1);
+        
+//         $v0->createEdgeTo($v1)->setCapacity(3.4);
+//         $v0->createEdgeTo($v1)->setCapacity(6.6);
+        
+//         $alg = new AlgorithmMaxFlowEdmondsKarp($v0,$v1);
+        
+//         $this->assertEquals(10,$alg->getFlowMax());
+//     }
+    
+//     public function testEdgesUndirected(){
+//     	$graph = new Graph();
+//     	$v0 = $graph->createVertex(0);
+//     	$v1 = $graph->createVertex(1);
+    
+//     	$v1->createEdge($v0)->setCapacity(7);
+    
+//     	$alg = new AlgorithmMaxFlowEdmondsKarp($v0,$v1);
+    
+//     	$this->assertEquals(7,$alg->getFlowMax());
+//     }
     
     /**
      * run algorithm with bigger graph and check result against known result (will take several seconds)
