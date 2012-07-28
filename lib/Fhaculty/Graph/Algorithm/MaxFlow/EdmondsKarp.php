1cad27a22985a83a3e2a387826280082493a2baf
Christian Lück <christian@lueck.tv>
2012-07-28 14:03:34 +0200

    Improve code readability

--- lib/Fhaculty/Graph/Algorithm/MaxFlow/EdmondsKarp.php
+++ lib/Fhaculty/Graph/Algorithm/MaxFlow/EdmondsKarp.php
@@ -51,6 +51,7 @@ class EdmondsKarp extends Base{
     public function createGraph(){
         $graphResult = $this->startVertex->getGraph()->createGraphClone();
         
+        // initialize null flow and check edges
         foreach ($graphResult->getEdges() as $edge){
             if(!($edge instanceof EdgeDirected)){
                 throw new UnexpectedValueException('Undirected edges not supported for edmonds karp');
@@ -58,34 +59,36 @@ class EdmondsKarp extends Base{
         	$edge->setFlow(0);
         }
         
+        $idA = $this->startVertex->getId();
+        $idB = $this->destinationVertex->getId();
+        
         do{
+            // Generate new residual graph and repeat
             $residualAlgorithm = new ResidualGraph($graphResult);
-            $graphResidual = $residualAlgorithm->createGraph(true);        // Generate new residual graph and repeat
-            
-            $startVertex = $graphResidual->getVertex($this->startVertex->getId());
-            $destinationVertex = $graphResidual->getVertex($this->destinationVertex->getId());
+            $graphResidual = $residualAlgorithm->createGraph();
             
             // 1. Search _shortest_ (number of hops and cheapest) path from s -> t
-            $breadthSearchAlg = new SearchBreadthFirst($startVertex);
-            $pathFlow = $breadthSearchAlg->getGraphPathTo($destinationVertex);  // Get shortest path if NULL-> Done
+            $breadthSearchAlg = new SearchBreadthFirst($graphResidual->getVertex($idA));
+            $pathFlow = $breadthSearchAlg->getGraphPathTo($graphResidual->getVertex($idB));
 
             if($pathFlow){                                                        // If path exists add the new flow to graph
                 // 2. get max flow from path
                 $maxFlowValue = Edge::getFirst($pathFlow->getEdges(),Edge::ORDER_CAPACITY)->getCapacity();
 
+                // 3. add flow to path
                 foreach ($pathFlow->getEdges() as $edge){
-                    try{
+                    try{ // try to look for forward edge to increase flow
                         $originalEdge = $graphResult->getEdgeClone($edge);
                         $originalEdge->setFlow($originalEdge->getFlow() + $maxFlowValue);
                     }
-                    catch(UnderflowException $e){
+                    catch(UnderflowException $e){ // forward edge not found, look for back edge to decrease flow
                         $originalEdge = $graphResult->getEdgeCloneInverted($edge);
                         $originalEdge->setFlow($originalEdge->getFlow() - $maxFlowValue);
                     }
                 }
             }
 
-        } while($pathFlow);
+        } while($pathFlow); // repeat while we still finds paths with residual capacity to add flow to
         
         return $graphResult;
     }
