004d80d24fbb7e4f50d9bc009fd86affed33aeb4
Christian Lück <christian@lueck.tv>
2012-08-15 00:19:11 +0200

    Explicitly create directed edges for temporary flow graph

--- lib/Fhaculty/Graph/Algorithm/MaximumMatching/Flow.php
+++ lib/Fhaculty/Graph/Algorithm/MaximumMatching/Flow.php
@@ -2,6 +2,10 @@
 
 namespace Fhaculty\Graph\Algorithm\MaximumMatching;
 
+use Fhaculty\Graph\Exception\LogicException;
+
+use Fhaculty\Graph\Exception\UnexpectedValueException;
+
 use Fhaculty\Graph\Algorithm\MaxFlow\EdmondsKarp as MaxFlowEdmondsKarp;
 use Fhaculty\Graph\Algorithm\Groups;
 use Fhaculty\Graph\Exception;
@@ -10,16 +14,16 @@ class Flow extends Base {
 
     public function getEdges() {
 //         if($this->graph->isDirected()){
-//             throw new Exception('Input graph must not be directed');
+//             throw new UnexpectedValueException('Input graph contains directed edges');
 //         }
         
         $alg = new Groups($this->graph);
         if(!$alg->isBipartit()){
-            throw new Exception\UnexpectedValueException('Input graph does not have bipartit groups assigned to each vertex. Consider Using "AlgorithmBipartit::createGraph()" first');
+            throw new UnexpectedValueException('Input graph does not have bipartit groups assigned to each vertex. Consider Using "AlgorithmBipartit::createGraph()" first');
         }
         
         // create temporary flow graph with supersource and supersink
-        $graphFlow = $this->graph->createGraphClone();
+        $graphFlow = $this->graph->createGraphCloneEdgeless();
 
         $vertices = $graphFlow->getVertices(); // get all vertices
         // above $vertices does NOT contain supersource and supersink, because
@@ -37,17 +41,18 @@ class Flow extends Base {
             $group = $vertex->getGroup();
             
             if($group === $groupA){ // source
-                $superSource->createEdgeTo($vertex);
+                $superSource->createEdgeTo($vertex)->setCapacity(1)->setFlow(0);
+                
+                // temporarily create edges from A->B for flow graph
+                $originalVertex = $this->graph->getVertex($vertex->getId());
+                foreach($originalVertex->getVerticesEdgeTo() as $vertexTarget){
+                    $vertex->createEdgeTo($graphFlow->getVertex($vertexTarget->getId()))->setCapacity(1)->setFlow(0);
+                }
             } else if($group === $groupB){ // sink
-                $vertex->createEdgeTo($superSink);
+                $vertex->createEdgeTo($superSink)->setCapacity(1)->setFlow(0);
             } else {
-                throw new Exception\LogicException('Should not happen. Unknown set: ' + $belongingSet);
+                throw new LogicException('Should not happen. Unknown set: ' + $belongingSet);
             }
-        }
-
-        // All capacities to 1 (according to algorithm)
-        foreach ($graphFlow->getEdges() as $edge){
-            $edge->setCapacity(1)->setFlow(0);
         }
         
 //         visualize($resultGraph);
