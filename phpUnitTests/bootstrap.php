ace23372dc6ea6d9670d19a40ac11768e8baf03c
Christian Lück <christian@lueck.tv>
2012-10-27 13:14:55 +0200

    Use composer autoloader (or complain if missing)

--- phpUnitTests/bootstrap.php
+++ phpUnitTests/bootstrap.php
@@ -1,3 +1,3 @@
 <?php
 
-require_once __DIR__.'/../example/init.inc.php';
+(include_once __DIR__.'/../vendor/autoload.php') OR die(PHP_EOL.'ERROR: composer autoloader not found, run "composer install" or see README for instructions'.PHP_EOL);
