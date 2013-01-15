f7af5718f0e84d03d2b84a8cb9bc63eeee87f18b
Christian Lück <christian@lueck.tv>
2013-01-15 07:32:29 +0100

    Additional unit tests for cloning

--- tests/Fhaculty/Graph/GraphTest.php
+++ tests/Fhaculty/Graph/GraphTest.php
@@ -14,6 +14,61 @@ class GraphTest extends TestCase
         $this->graph = new Graph();
     }
     
+    public function testVertexClone(){
+        $graph = new Graph();
+        $vertex = $graph->createVertex(123)->setBalance(10)->setGroup(4);
+    
+        $newgraph = new Graph();
+        $newvertex = $newgraph->createVertexClone($vertex);
+    
+        $this->assertVertexEquals($vertex,$newvertex);
+    }
+    
+    /**
+     * test to make sure vertex can not be cloned into same graph (due to duplicate id)
+     *
+     * @expectedException RuntimeException
+     */
+    public function testInvalidVertexClone(){
+        $graph = new Graph();
+        $vertex = $graph->createVertex(123);
+        $graph->createVertexClone($vertex);
+    }
+    
+    public function testGraphCloneEmpty(){
+        $graph = new Graph();
+        $newgraph = $graph->createGraphClone();
+        $this->assertGraphEquals($graph,$newgraph);
+    }
+    
+    public function testGraphClone(){
+        $graph = new Graph();
+        $graph->createVertex(123)->setBalance(10)->setGroup(4);
+        
+        $newgraph = $graph->createGraphClone();
+    
+        $this->assertGraphEquals($graph,$newgraph);
+        
+        $graphClonedTwice = $newgraph->createGraphClone();
+        
+        $this->assertGraphEquals($graph, $graphClonedTwice);
+    }
+    
+    public function testGraphCloneEdgeless(){
+        $graph = new Graph();
+        $graph->createVertex(1)->createEdgeTo($graph->createVertex(2));
+        $graph->createVertex(3)->createEdge($graph->getVertex(2));
+    
+        $graphEdgeless = $graph->createGraphCloneEdgeless();
+    
+        $graphExpected = new Graph();
+        $graphExpected->createVertex(1);
+        $graphExpected->createVertex(2);
+        $graphExpected->createVertex(3);
+    
+        $this->assertGraphEquals($graphExpected,$graphEdgeless);
+    }
+    
     public function testBalance(){
         $this->assertEquals(0,$this->graph->getBalance());
     }
