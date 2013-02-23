0caf6c4ff6c864344fb01058518e69eef8c66932
Christian Lück <christian@lueck.tv>
2013-02-23 01:41:17 +0100

    Sample image exporter

---
+++ lib/Fhaculty/Graph/Exporter/Image.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Fhaculty\Graph\Exporter;
+
+use Fhaculty\Graph\GraphViz;
+use Fhaculty\Graph\Graph;
+
+class Image implements ExporterInterface
+{
+    private $format = 'png';
+    
+    public function getOutput(Graph $graph)
+    {
+        $graphviz = new GraphViz($graph);
+        $graphviz->setFormat($this->format);
+        return $graphviz->createImageData();
+    }
+    
+    /**
+     * set the image output format to use
+     * 
+     * @param string $type png, svg
+     * @return self $this (chainable)
+     * @see GraphViz::setFormat()
+     */
+    public function setFormat($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+}
