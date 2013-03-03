c9e6f3f45d3f25b56f63b1275d8ac2c173610878
Clemens Tolboom <clemens@build2be.com>
2013-03-03 16:36:02 +0100

    Some more tests.

--- tests/Fhaculty/Graph/Algorithm/TopologicalSortTest.php
+++ tests/Fhaculty/Graph/Algorithm/TopologicalSortTest.php
@@ -52,6 +52,85 @@ class TopologicalSortTest extends TestCase
     }
 
     /**
+     * We test a more complex graph.
+     *
+     * A -> B -> C
+     * P -> Q
+     * B -> Q
+     * C -> P
+     *
+     * should result into
+     *
+     * A -> B -> C
+     *              P -> Q
+     */
+    public function testGraphComplexInOrder() {
+        $graph = new Graph();
+        $graph->createVertex("A")->createEdgeTo($graph->createVertex("B"));
+
+        $alg = new TopologicalSort($graph);
+
+        $tsl = array_keys($alg->getVertices());
+        $this->assertSame(array('A', 'B'), $tsl);
+
+        $graph->getVertex("B")->createEdgeTo($graph->createVertex("C"));
+        $tsl = array_keys($alg->getVertices());
+        $this->assertSame(array('A', 'B', 'C'), $tsl);
+
+        $graph->createVertex("P")->createEdgeTo($graph->createVertex("Q"));
+        $tsl = array_keys($alg->getVertices());
+        $this->assertSame(array('A', 'B', 'C', 'P', 'Q'), $tsl);
+
+        $graph->getVertex("B")->createEdgeTo($graph->getVertex("Q"));
+        $tsl = array_keys($alg->getVertices());
+        $this->assertSame(array('A', 'B', 'C', 'P', 'Q'), $tsl, 'Added B -> Q');
+
+        $graph->getVertex("C")->createEdgeTo($graph->getVertex("P"));
+        $tsl = array_keys($alg->getVertices());
+        $this->assertSame(array('A', 'B', 'C', 'P', 'Q'), $tsl, "Complete Graph");
+    }
+
+    /**
+     * We test a more complex graph.
+     *
+     * A -> B -> C
+     * Q -> B
+     * A -> P
+     *
+     * should result into a TSL
+     *
+     * A ->         B -> C
+     *      P -> Q
+     */
+    public function testGraphComplexOutOfOrder() {
+        $graph = new Graph();
+        $graph->createVertex("A")->createEdgeTo($graph->createVertex("B"));
+
+        $alg = new TopologicalSort($graph);
+
+        $tsl = array_keys($alg->getVertices());
+        $this->assertSame(array('A', 'B'), $tsl);
+
+        $graph->getVertex("B")->createEdgeTo($graph->createVertex("C"));
+        $tsl = array_keys($alg->getVertices());
+        $this->assertSame(array('A', 'B', 'C'), $tsl);
+
+        $graph->createVertex("P")->createEdgeTo($graph->createVertex("Q"));
+        $tsl = array_keys($alg->getVertices());
+        $this->assertSame(array('A', 'B', 'C', 'P', 'Q'), $tsl);
+
+        $graph->getVertex("Q")->createEdgeTo($graph->getVertex("B"));
+        $tsl = array_keys($alg->getVertices());
+        $this->assertSame(array('A', 'P', 'Q', 'B', 'C'), $tsl, 'Added Q -> B');
+
+        $graph->getVertex("A")->createEdgeTo($graph->getVertex("P"));
+        $tsl = array_keys($alg->getVertices());
+        $this->assertSame(array('A', 'P', 'Q', 'B', 'C'), $tsl, 'Complete Graph');
+    }
+
+    /**
+     * 1 - 2 : undirected
+     *
      * @expectedException UnexpectedValueException
      */
     public function testFailUndirected()
