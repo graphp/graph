b91ea7fa4a779d5d5f70db39718b3bd83b096920
Christian Lück <christian@lueck.tv>
2013-06-16 23:40:18 +0200

    Move Vertex degree helpers to Algorithm\Degree

--- lib/Fhaculty/Graph/Algorithm/Degree.php
+++ lib/Fhaculty/Graph/Algorithm/Degree.php
@@ -24,17 +24,17 @@ class Degree extends BaseGraph
      * @return int
      * @throws UnderflowException       if graph is empty
      * @throws UnexpectedValueException if graph is not regular (i.e. vertex degrees are not equal)
-     * @uses Vertex::getDegree()
+     * @uses self::getDegreeVertex()
      * @see self::isRegular()
      */
     public function getDegree()
     {
         // get initial degree of any start vertex to compare others to
-        $degree = $this->graph->getVertexFirst()->getDegree();
+        $degree = $this->getDegreeVertex($this->graph->getVertexFirst());
 
         foreach ($this->graph->getVertices() as $vertex) {
             /** @var $vertex Vertex */
-            $i = $vertex->getDegree();
+            $i = $this->getDegreeVertex($vertex);
 
             if ($i !== $degree) {
                 throw new UnexpectedValueException('Graph is not k-regular (vertex degrees differ)');
@@ -50,11 +50,11 @@ class Degree extends BaseGraph
      * @return int
      * @throws Exception if graph is empty or directed
      * @uses Vertex::getFirst()
-     * @uses Vertex::getDegree()
+     * @uses self::getDegreeVertex()
      */
     public function getDegreeMin()
     {
-        return Vertex::getFirst($this->graph->getVertices(), Vertex::ORDER_DEGREE)->getDegree();
+        return $this->getDegreeVertex(Vertex::getFirst($this->graph->getVertices(), Vertex::ORDER_DEGREE));
     }
 
     /**
@@ -63,18 +63,18 @@ class Degree extends BaseGraph
      * @return int
      * @throws Exception if graph is empty or directed
      * @uses Vertex::getFirst()
-     * @uses Vertex::getDegree()
+     * @uses self::getDegreeVertex()
      */
     public function getDegreeMax()
     {
-        return Vertex::getFirst($this->graph->getVertices(), Vertex::ORDER_DEGREE, true)->getDegree();
+        return $this->getDegreeVertex(Vertex::getFirst($this->graph->getVertices(), Vertex::ORDER_DEGREE, true));
     }
 
     /**
      * checks whether this graph is regular, i.e. each vertex has the same indegree/outdegree
      *
      * @return boolean
-     * @uses Graph::getDegree()
+     * @uses self::getDegree()
      */
     public function isRegular()
     {
@@ -95,17 +95,125 @@ class Degree extends BaseGraph
      * checks whether the indegree of every vertex equals its outdegree
      *
      * @return boolean
-     * @uses Vertex::getDegreeIn()
-     * @uses Vertex::getDegreeOut()
+     * @uses self::getDegreeInVertex()
+     * @uses self::getDegreeOutVertex()
      */
     public function isBalanced()
     {
         foreach ($this->graph->getVertices() as $vertex) {
-            if ($vertex->getDegreeIn() !== $vertex->getDegreeOut()) {
+            if ($this->getDegreeInVertex($vertex) !== $this->getDegreeOutVertex($vertex)) {
                 return false;
             }
         }
 
         return true;
+    }
+
+    /**
+     * checks whether this vertex is a source, i.e. its indegree is zero
+     *
+     * @param Vertex $vertex
+     * @return boolean
+     * @uses Edge::hasVertexTarget()
+     * @see self::getDegreeInVertex()
+     */
+    public function isVertexSource(Vertex $vertex)
+    {
+        foreach ($vertex->getEdges() as $edge) {
+            if ($edge->hasVertexTarget($vertex)) {
+                return false;
+            }
+        }
+
+        // reach this point: no edge to this vertex
+        return true;
+    }
+
+    /**
+     * checks whether this vertex is a sink, i.e. its outdegree is zero
+     *
+     * @param Vertex $vertex
+     * @return boolean
+     * @uses Edge::hasVertexStart()
+     * @see self::getDegreeOutVertex()
+     */
+    public function isVertexSink(Vertex $vertex)
+    {
+        foreach ($vertex->getEdges() as $edge) {
+            if ($edge->hasVertexStart($vertex)) {
+                return false;
+            }
+        }
+
+        // reach this point: no edge away from this vertex
+        return true;
+    }
+
+    /**
+     * get degree of this vertex (total number of edges)
+     *
+     * vertex degree counts the total number of edges attached to this vertex
+     * regardless of whether they're directed or not. loop edges are counted
+     * twice as both start and end form a 'line' to the same vertex.
+     *
+     * @param Vertex $vertex
+     * @return int
+     * @see self::getDegreeInVertex()
+     * @see self::getDegreeOutVertex()
+     */
+    public function getDegreeVertex(Vertex $vertex)
+    {
+        return count($vertex->getEdges());
+    }
+
+    /**
+     * check whether this vertex is isolated (i.e. has no edges attached)
+     *
+     * @param Vertex $vertex
+     * @return boolean
+     */
+    public function isVertexIsolated(Vertex $vertex)
+    {
+        return !$vertex->getEdges();
+    }
+
+    /**
+     * get indegree of this vertex (number of edges TO this vertex)
+     *
+     * @param Vertex $vertex
+     * @return int
+     * @uses Edge::hasVertexTarget()
+     * @see self::getDegreeVertex()
+     */
+    public function getDegreeInVertex($vertex)
+    {
+        $n = 0;
+        foreach ($vertex->getEdges() as $edge) {
+            if ($edge->hasVertexTarget($vertex)) {
+                ++$n;
+            }
+        }
+
+        return $n;
+    }
+
+    /**
+     * get outdegree of this vertex (number of edges FROM this vertex TO other vertices)
+     *
+     * @param Vertex $vertex
+     * @return int
+     * @uses Edge::hasVertexStart()
+     * @see self::getDegreeVertex()
+     */
+    public function getDegreeOutVertex(Vertex $vertex)
+    {
+        $n = 0;
+        foreach ($vertex->getEdges() as $edge) {
+            if ($edge->hasVertexStart($vertex)) {
+                ++$n;
+            }
+        }
+
+        return $n;
     }
 }
